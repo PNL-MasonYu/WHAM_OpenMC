5aaeb4109d5e77754bc23e1c18d97b375d2fa6c4
myu233 <myu233@hpclogin1.chtc.wisc.edu>
2021-07-18 02:25:09 -0500

    running on cluster

--- wham_openmc.sh
+++ wham_openmc.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #This file is called submit-script.sh
-#SBATCH --partition=univ2       # default "univ2", if not specified
-#SBATCH --time=2-00:00:00       # run time in days-hh:mm:ss
-#SBATCH --nodes=4               # require 2 nodes
+#SBATCH --partition=pre       # default "univ2", if not specified
+#SBATCH --time=1-00:00:00       # run time in days-hh:mm:ss
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=20    # cpus per node (by default, "ntasks"="cpus")
-#SBATCH --mem=128000             # RAM per node
-#SBATCH --error=job.%J.err
-#SBATCH --output=job.%J.out
+#SBATCH --mem=64000             # RAM per node
+#SBATCH --error=job.err
+#SBATCH --output=job.out
 # Make sure to change the above two lines to reflect your appropriate
 # file locations for standard error and output
 
 # Now list your executable command (or a string of them).
 # Example for code compiled with a software module:
-module load openmpi
-mpirun -n 80 /software/openmc/build/bin/openmc
+module load openmpi hdf5
+mpirun -n 4 /software/myu233/openmc/build/bin/openmc -s 20
