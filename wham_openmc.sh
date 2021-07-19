1dddd7249e9fe79e1634bd80b0c19a57e1e8eb48
myu233 <myu233@hpclogin1.chtc.wisc.edu>
2021-07-19 08:21:32 -0500

    Issues with OpenMPI vs OpenMP parallelization

--- wham_openmc.sh
+++ wham_openmc.sh
@@ -2,7 +2,7 @@
 #This file is called submit-script.sh
 #SBATCH --partition=pre       # default "univ2", if not specified
 #SBATCH --time=1-00:00:00       # run time in days-hh:mm:ss
-#SBATCH --nodes=4
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=20    # cpus per node (by default, "ntasks"="cpus")
 #SBATCH --mem=64000             # RAM per node
 #SBATCH --error=job.err
@@ -13,4 +13,5 @@
 # Now list your executable command (or a string of them).
 # Example for code compiled with a software module:
 module load openmpi hdf5
-mpirun -n 4 /software/myu233/openmc/build/bin/openmc -s 20
+#/software/myu233/openmc/build/bin/openmc -s 20
+mpiexec -n 8 --bind-to numa /software/myu233/openmc/build/bin/openmc -s 20
